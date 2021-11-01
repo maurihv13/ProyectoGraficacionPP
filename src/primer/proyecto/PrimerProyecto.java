8597561bb4245d0e64acca1365ad4f263f1803dc
Juan Pablo <Juan Pablo@LAPTOP-1QPG05PN>
2021-11-01 10:22:22 -0400

    primero

---
+++ src/primer/proyecto/PrimerProyecto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package primer.proyecto;
+import java.util.Scanner;
+/**
+ *
+ * @author Juan Pablo
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x*20);
+    }
+
+}
