fdb24b1f16ca9525f81abe8027a6969b3fc88bc2
fred <alfredoLazaro999@gmail.com>
2021-11-14 19:36:10 -0400

    trate de hacer el algoritmo bresenham pero no parece funcionar

--- src/figuras/BresenhamLinea.java
+++ src/figuras/BresenhamLinea.java
@@ -6,9 +6,62 @@
 package figuras;
 
 /**
- *
- * @author USUARIO
+ *aun no funciona
+ * @author Fred
  */
 public class BresenhamLinea {
+   
+    public void algoritmo(int x0,int y0,int x1,int y1){
+        int dx,dy,contad,pasox,pasoy,x,y,p,incE,incNE;
+        int espacio=0;
+        int segment=0;
+        boolean lleno;
+        dx=x1-x0;
+        dy=y1-y0;
+        contad=0;
+        lleno=false;
+        pasox=1;
+        pasoy=1;
+        if (dx<0) {
+            dx=-dx;
+            pasox=-1;
+        }else{
+            pasoy=1;
+        }
+        if(dy<0){
+            dy=-dy;
+            pasoy=-1;
+        }else{
+            pasoy=1;
+        }
+        x=x0;
+        y=y0;
+        
+        if(dx>dy){
+            p=2*dy-dx;
+            incE=2*dy;
+            incNE=2*(dy-dx);
+            
+            while(x!=x1){
+                x=x+pasox;
+                if(p<0){
+                    p=p+incE;
+                }else{
+                    y=y+pasoy;
+                    p=p+incNE;
+                    
+                }
+                if(espacio!=0){
+                    if(lleno){
+                        if(contad<segment){
+                            boolean arrib=true;
+                            //aqui pinta
+                        }
+                    }else{}
+                }else{}
+            }
+        }else{}
+        
+    }
     
 }
